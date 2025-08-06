13fca634172a6fa6be3b7251b59145c4b1b44e07
Fuki0612 <fukimax0612@gmail.com>
2025-08-06 18:25:10 +0900

    タイトル課題作成

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,11 +1,75 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _currentTime = '';
+  Timer? _timer;
+
+  void _updateTime() {
+    final now = DateTime.now(); // 現在の時刻を取得
+    setState(() {
+      _currentTime =
+          '${now.hour.toString().padLeft(2, '0')}:' //Hourの表示
+          '${now.minute.toString().padLeft(2, '0')}'; //Minuteの表示
+      // タイトル タスク3 Secondsの表示
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+      _updateTime();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('Home Page')),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            // タイトル タスク4 ここにdecorationを追加
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  '現在時刻',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  _currentTime,
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
