16f616d871d05a61f447b6c84920041892f18e1b
natsuki431 <natsukichi12345@gmail.com>
2025-08-16 20:56:26 +0900

    feat:main.dartとpage.dartの修正

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'pages/home/page.dart';
+import 'pages/alarm/page.dart';
+import 'pages/collection/page.dart';
+import 'pages/game/page.dart';
+import 'pages/chat/page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const TitlePage());
+}
+
+class TitlePage extends StatelessWidget {
+  const TitlePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Practice',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const MyTitlePage(title: '1 minute alarm'),
+    );
+  }
+}
+
+class MyTitlePage extends StatefulWidget {
+  const MyTitlePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyTitlePage> createState() => _MyTitlePageState();
+}
+
+class _MyTitlePageState extends State<MyTitlePage> {
+  int _selectedIndex = 0;
+
+  Widget _pages() {
+    switch (_selectedIndex) {
+      case 0: // Home
+        return HomePage();
+      case 1: // Alarm
+        return AlarmPage();
+      case 2: // Chat
+        return ChatPage();
+      case 3: // Collection
+        return CollectionPage();
+      case 4: // Game
+        return GamePage();
+      default:
+        return Center(child: Text('Page $_selectedIndex'));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages(),
+      bottomNavigationBar: ConvexAppBar(
+        // タイトル タスク.1
+        style: TabStyle.fixed,
+        color: Color(0xFF81D4FA),
+        backgroundColor: Color(0xFF2196F3),
+        activeColor: Color(0xFFFFFFFF),
+        curveSize: 0,
+        height: 80,
+        items: [
+          // タイトル タスク.2
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.alarm_on, title: 'Alarm'),
+          TabItem(icon: Icons.chat, title: 'Chat'),
+          TabItem(icon: Icons.photo_album, title: 'Collection'),
+          TabItem(icon: Icons.smart_toy, title: 'Game'),
+        ],
+        initialActiveIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _currentTime = '';
+  Timer? _timer;
+
+  void _updateTime() {
+    final now = DateTime.now(); // 現在の時刻を取得
+    setState(() {
+      _currentTime =
+          '${now.hour.toString().padLeft(2, '0')}:' //Hourの表示
+          '${now.minute.toString().padLeft(2, '0')}' //Minuteの表示
+          '${now.second.toString().padLeft(2, '0')}';
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+      _updateTime();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            // タイトル タスク4 ここにdecorationを追加
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  '現在時刻',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  _currentTime,
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
