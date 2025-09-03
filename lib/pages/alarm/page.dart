becf05a9fef7b1cf6996d1595d76c0a64227054d
Barnum8639 <sutanai1925@gmail.com>
2025-09-03 16:38:06 +0900

    Timepickerで時刻選択を可能に

--- lib/pages/alarm/page.dart
+++ lib/pages/alarm/page.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
-import 'notification.dart';
+import 'functions/notification.dart';
+import 'functions/timeselect.dart';
 
 void init() async {
   // 通知サービスの初期化
   await SimpleNotificationService.initialize();
 }
 
-class AlarmPage extends StatelessWidget {
+class AlarmPage extends StatefulWidget {
   const AlarmPage({super.key});
+
+  @override
+  State<AlarmPage> createState() => _AlarmPageState();
+}
+
+class _AlarmPageState extends State<AlarmPage> {
+  TimeOfDay _time = TimeOfDay.now();
+  final TimePickerService _timePickerService = TimePickerService();
+
+  Future<void> _handleTimeSelection() async {
+    final newTime = await _timePickerService.pickTime(context, _time);
+    if (newTime != null) {
+      setState(() => _time = newTime);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotificationScreen(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: ElevatedButton(
+                child: Text(_timePickerService.formatTime(_time)),
+                onPressed: _handleTimeSelection,
+              ),
+            ),
+            const Expanded(
+              child: NotificationScreen(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
